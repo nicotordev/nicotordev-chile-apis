04dbe487b9c4f0a6bd38b1f43e9507a3354cf1e8
Nicolas <nicotordev@gmail.com>
2025-03-25 19:56:34 -0300

    feat: add new constant for Chilean news prompt category in AI constants

--- src/constants/ai.constants.ts
+++ src/constants/ai.constants.ts
@@ -1,6 +1,7 @@
 const aiConstants = {
   DEFAULT_MODEL: 'models/gemini-2.0-flash-001',
   REMOVED_CATEGORY_PROMPS_NAME: 'Removed Prompt Categories',
+  CHILEAN_NEWS_PROMPT_CATEGORY_NAME: 'noticias-chile-scrapper',
 };
 
 export default aiConstants;
