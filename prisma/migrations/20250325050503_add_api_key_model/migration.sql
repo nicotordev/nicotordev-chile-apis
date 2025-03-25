d640f1458c462c8756aac7de1dffc719a2f33455
Nicolas <nicotordev@gmail.com>
2025-03-25 02:10:00 -0300

    feat: add ApiKey model to database schema and migration

---
+++ prisma/migrations/20250325050503_add_api_key_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "ApiKey" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ApiKey_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ApiKey_key_key" ON "ApiKey"("key");
